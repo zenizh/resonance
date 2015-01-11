249bcda9399af557dee15fc116cf507bd47c6952
kami <hiroki.zenigami@gmail.com>
2015-01-11 18:09:05 +0900

    In development

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe Post, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Post, type: :model do
+  let(:user) { User.create(name: 'hoge') }
+  let(:post) { Post.create() }
+
+  describe 'instance methods' do
+    it 'should be defined' do
+      # source: :user, with: :like
+      expect(post).not_to respond_to(:like)
+      expect(post).not_to respond_to(:unlike)
+      expect(post).not_to respond_to(:liking?)
+      expect(post).not_to respond_to(:liking)
+      expect(post).to respond_to(:liked_by?)
+      expect(post).to respond_to(:likers)
+    end
+  end
+
+  describe '#liked_by?' do
+    before { user.like post }
+
+    it 'should be liked by user' do
+      expect(post).to be_liked_by(user)
+    end
+  end
+
+  describe '#likers' do
+    before { user.like post }
+
+    it 'should include user' do
+      expect(post.likers).to be_exists(id: user.id)
+    end
+  end
 end
