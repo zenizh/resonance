19a2758f007266b635af8fe4b80c34be587e057b
kami <kami30k@gmail.com>
2015-01-11 18:09:05 +0900

    In development

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,25 +10,33 @@ describe User, type: :model do
       expect(user).to respond_to(:follow)
       expect(user).to respond_to(:unfollow)
       expect(user).to respond_to(:following?)
-      expect(user).to respond_to(:followed_by?)
       expect(user).to respond_to(:following)
+      expect(user).to respond_to(:followed_by?)
       expect(user).to respond_to(:followers)
 
       # with: :block
       expect(user).to respond_to(:block)
       expect(user).to respond_to(:unblock)
       expect(user).to respond_to(:blocking?)
-      expect(user).to respond_to(:blocked_by?)
       expect(user).to respond_to(:blocking)
+      expect(user).to respond_to(:blocked_by?)
       expect(user).to respond_to(:blockers)
 
       # with: :mute
       expect(user).to respond_to(:mute)
       expect(user).to respond_to(:unmute)
       expect(user).to respond_to(:muting?)
-      expect(user).to respond_to(:muted_by?)
       expect(user).to respond_to(:muting)
+      expect(user).to respond_to(:muted_by?)
       expect(user).to respond_to(:muters)
+
+      # target: :post, with: :like
+      expect(user).to respond_to(:like)
+      expect(user).to respond_to(:unlike)
+      expect(user).to respond_to(:liking?)
+      expect(user).to respond_to(:liking)
+      expect(user).not_to respond_to(:liked_by?)
+      expect(user).not_to respond_to(:likers)
     end
   end
 
@@ -88,14 +96,6 @@ describe User, type: :model do
     end
   end
 
-  describe '#followed_by?' do
-    before { other_user.follow user }
-
-    it 'should be followed by other user' do
-      expect(user).to be_followed_by(other_user)
-    end
-  end
-
   describe '#following' do
     before { user.follow other_user }
 
@@ -104,10 +104,18 @@ describe User, type: :model do
     end
   end
 
+  describe '#followed_by?' do
+    before { other_user.follow user }
+
+    it 'should be followed by other user' do
+      expect(user).to be_followed_by(other_user)
+    end
+  end
+
   describe '#followers' do
     before { other_user.follow user }
 
-    it 'should be followed by other_user' do
+    it 'should include other_user' do
       expect(user.followers).to be_exists(other_user.id)
     end
   end
