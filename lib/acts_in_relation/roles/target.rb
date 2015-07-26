0de48cff259a70ebadb1881ac64cad1c570b30c0
Shohei Kameda <shoheik@cpan.org>
2015-07-27 08:55:53 +0900

    chnaged target#action_by to use tartget id

--- lib/acts_in_relation/roles/target.rb
+++ lib/acts_in_relation/roles/target.rb
@@ -14,7 +14,7 @@ module ActsInRelation
               source: :"#{source}"
 
             def #{pastize(action)}_by?(source)
-              source.#{action.pluralize}.exists?(#{source}_id: source.id)
+              source.#{action.pluralize}.exists?(target_#{target}_id: id) 
             end
           RUBY
         end
